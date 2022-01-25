6692f16a346173fc8c85ea4716ec41be508fa657
Shinjiwoooo <yuljeonjww@gmail.com>
2022-01-25 22:21:30 +0900

    2022.01.25

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
-import {Navbar, Container, Nav, NavDropdown,jumbotron,button} from 'react-bootstrap'
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
 import { Button } from 'react-bootstrap';
 import './App.css';
-
+import React, { useState } from 'react';
+import Data from './data.js'
+import Detail from './Detail.js'
+import {Link, Route, Switch} from 'react-router-dom'
 
 function App() {
+ 
+  let [ring, ring변경] = useState(Data)
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
@@ -13,8 +19,8 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -26,12 +32,36 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+    
 
-      <JumboTron></JumboTron>
 
+    <Switch>
+    
+      <Route exact path ="/">
+        <JumboTron></JumboTron>
+        <div className="container">
+        <div className="row">
+          {
+            ring.map((item, index)=>{
+              return <Card ring={ring[index]}  i={index} key={index}></Card>
+            })
+          }
+        </div>
+      </div>
+      </Route>
+      <Route path="/detail/:id">
+          <Detail ring={ring}></Detail>
+      </Route>
+
+      <Route path="/:id">
+          <div>아무개</div>
+      </Route>
+    </Switch>
+      
     </div>
   );
 }
+
 
 function JumboTron() {
   return (
@@ -42,6 +72,17 @@ function JumboTron() {
           <Button variant="primary">Learn more</Button>
         </p>
       </div>
+    </div>
+  )
+}
+
+function Card(props){
+  return(
+    <div className="col-md-4">
+      <img src={"https://dongmyounglee.github.io/img/ring0"+(props.i + 1)+".png"} width="100%" />
+      <h4>{props.ring.title}</h4>  
+      <p>{props.ring.content}</p>
+      <p>{props.ring.price}</p>
     </div>
   )
 }
---
+++ shoppingmall/src/Detail.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components'
+import './Detail.scss'
+
+let 박스 = styled.div`
+    padding-top:30px;
+`
+
+let 제목 = styled.h4`
+    font-size:25px;
+`
+
+function Detail(props) {
+    let [alert, alert변경] = useState(true)
+    useEffect(()=>{
+        let 타이머 = setTimeout(()=>{
+            alert변경(false)
+          console.log('2초지남')
+        }, 2000)
+    },[alert])
+
+    
+
+    let { id } =useParams();
+    let history =useHistory();
+
+    let 찾은상품 = props.ring.find(function(상품){
+        return 상품.id == id
+    })
+
+    return (
+      <div className="container">
+          <박스>
+            <제목 className="red">Detail</제목>
+          </박스>
+          
+          {
+              alert === true
+              ? (<div className="my-alert">
+              <p>재고가 얼마 남지 않았습니다</p>
+              </div>)
+              :null
+          }
+        <div className="row">
+          <div className="col-md-6">
+            <img src={"https://dongmyounglee.github.io/img/ring0"+(찾은상품.id+1)+".png"} width="100%" />
+          </div>
+          <div className="col-md-6 mt-4">
+            <h4 className="pt-5">{찾은상품.title}</h4>
+            <p>{찾은상품.content}</p>
+            <p>{찾은상품.price}</p>
+            <button className="btn btn-danger">주문하기</button> 
+            <button className="btn btn-danger onClick" onClick={()=>{
+                history.push('/')
+            }}>뒤로가기</button> 
+          </div>
+        </div>
+      </div>
+    )
+  }
+  export default Detail
